ba7431fcd6027bfc1de5dff9efb8b631a65fe121
Uthaeus <romanlavery@gmail.com>
2024-01-05 11:34:24 -0700

    added initial styling to main navigation and homepage

--- src/components/navigation/main-navigation.js
+++ src/components/navigation/main-navigation.js
@@ -5,12 +5,12 @@ function MainNavigation() {
     return (
         <div className="main-navigation">
             <div className="main-navigation-left">
-                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/' className={({isActive}) => isActive ? 'main-nav-link active-link' : 'main-nav-link'}>Home</NavLink>
             </div>
 
             <div className="main-navigation-right">
-                <NavLink to='/auth/login'>Login</NavLink>
-                <NavLink to='/auth/signup'>Sign Up</NavLink>
+                <NavLink to='/auth/login' className={({isActive}) => isActive ? 'main-nav-link active-link' : 'main-nav-link'}>Login</NavLink>
+                <NavLink to='/auth/signup' className={({isActive}) => isActive ? 'main-nav-link active-link' : 'main-nav-link'}>Sign Up</NavLink>
             </div>
         </div>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ function Home() {
 
     return (
         <div className="home">
-            <h1>Home Page</h1>
+            <h1 className="home-title">Home Page</h1>
 
             <div className="home-recipe-list-container">
                 {DUMMY_DATA.map((item) => <RecipeItem {...item} key={item.id} />)}
